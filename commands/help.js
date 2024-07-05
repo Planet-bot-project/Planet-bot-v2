fa0dbdce445f962407069326d1effad6a84156e4
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-07-05 23:35:56 +0900

    不要なコマンドの削除と一部リファクタリング

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
       type: ApplicationCommandOptionType.String,
       name: "commands",
       description: "詳細を表示するコマンドを指定します",
-      required: true,
+      required: false,
       choices: [
         { name: "about", value: "about" },
         { name: "botadmin", value: "botadmin" },
@@ -17,74 +17,15 @@ module.exports = {
         { name: "qr_code", value: "qr_code" },
         { name: "tc_create", value: "tc_create" },
         { name: "yt_search", value: "yt_search" },
-        { name: "command_list", value: "command_list" },
       ],
     },
   ],
 
   run: async (client, interaction) => {
     try {
-      if (interaction.options.getString("commands") === "about") {
-        await interaction.reply({
-          embeds: [
-            {
-              title: "Aboutコマンドのヘルプ",
-              description: "このBOTに関する情報を表示します",
-              color: 4303284,
-              thumbnail: {
-                url: "attachment://logo.png",
-              },
-              timestamp: new Date(),
-            },
-          ],
-          files: [
-            {
-              attachment: "images/logo.png",
-              name: "logo.png",
-            },
-          ],
-        });
-      } else if (interaction.options.getString("commands") === "botadmin") {
-        await interaction.reply({
-          embeds: [
-            {
-              title: "Botadminコマンドのヘルプ",
-              description: "このBOTの管理者についてを表示します。",
-              color: 4303284,
-              thumbnail: {
-                url: "attachment://logo.png",
-              },
-              timestamp: new Date(),
-            },
-          ],
-          files: [
-            {
-              attachment: "images/logo.png",
-              name: "logo.png",
-            },
-          ],
-        });
-      } else if (interaction.options.getString("commands") === "omikuji") {
-        await interaction.reply({
-          embeds: [
-            {
-              title: "Omikujiコマンドのヘルプ",
-              description: "おみくじの結果を表示します。",
-              color: 4303284,
-              thumbnail: {
-                url: "attachment://logo.png",
-              },
-              timestamp: new Date(),
-            },
-          ],
-          files: [
-            {
-              attachment: "images/logo.png",
-              name: "logo.png",
-            },
-          ],
-        });
-      } else if (interaction.options.getString("commands") === "ping") {
+      const commands = interaction.options.getString("commands");
+
+      if (commands === "ping") {
         await interaction.reply({
           embeds: [
             {
@@ -104,7 +45,7 @@ module.exports = {
             },
           ],
         });
-      } else if (interaction.options.getString("commands") === "qr_code") {
+      } else if (commands === "qr_code") {
         await interaction.reply({
           embeds: [
             {
@@ -125,52 +66,11 @@ module.exports = {
             },
           ],
         });
-      } else if (interaction.options.getString("commands") === "tc_create") {
+      } else {
         await interaction.reply({
           embeds: [
             {
-              title: "Tc_createコマンドのヘルプ",
-              description:
-                "・カテゴリーチャンネル\n・テキストチャンネル\n・ボイスチャンネル\nを作成します。\n```diff\n- 要チャンネル管理権限！！ -\n```",
-              color: 4303284,
-              thumbnail: {
-                url: "attachment://logo.png",
-              },
-              timestamp: new Date(),
-            },
-          ],
-          files: [
-            {
-              attachment: "images/logo.png",
-              name: "logo.png",
-            },
-          ],
-        });
-      } else if (interaction.options.getString("commands") === "yt_search") {
-        await interaction.reply({
-          embeds: [
-            {
-              title: "Yt_searchコマンドのヘルプ",
-              description: "Youtubeの動画を検索します。",
-              color: 4303284,
-              thumbnail: {
-                url: "attachment://logo.png",
-              },
-              timestamp: new Date(),
-            },
-          ],
-          files: [
-            {
-              attachment: "images/logo.png",
-              name: "logo.png",
-            },
-          ],
-        });
-      } else if (interaction.options.getString("commands") === "command_list") {
-        await interaction.reply({
-          embeds: [
-            {
-              title: "コマンドリスト",
+              title: "Planet-botのヘルプ",
               description:
                 "`/ping`でPINGを見てみよう！\n\n`/botadmin`でこのBOTの管理者のメンションをするよ\n\n`/me`であなたにメンションするよ\n\n`/help`でこれを表示するよ\n\n`/omikuji`でおみくじを引けるよ\n\n`/tc_create <何を作るか選択> <チャンネル名>`でチャンネルを作成するよ\n　※要、チャンネル管理権限\n\n`/yt_search <キーワード>` でそのキーワードに関連した動画URLを投稿するよ\n\n`/qr_code <文字列かURL>`でその文字列を読み取れるQRコードを作成するよ\n　※QRコードは(株)デンソーウェーブの登録商標です。\n\n`/stop`でBOTを停止するよ(__**BOT管理者限定機能**__)",
               color: 4303284,
