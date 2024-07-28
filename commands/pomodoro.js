41eb319529d53f05188b52f8634e61d64779ea8e
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-07-28 18:57:40 +0900

    onコマンドの内容をちょっと追加

--- commands/pomodoro.js
+++ commands/pomodoro.js
@@ -1,4 +1,10 @@
-const { ApplicationCommandOptionType } = require("discord.js");
+const {
+  ApplicationCommandOptionType,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+const profileModel = require("../models/profileSchema");
 
 module.exports = {
   name: "pomodoro",
@@ -15,6 +21,8 @@ module.exports = {
           description:
             "タイマーを使用するカテゴリーを設定してください。設定しない場合は入力しないでください。",
           required: false,
+          //チャンネルタイプ指定が出来るようにする
+          //https://stackoverflow.com/questions/76114164
         },
       ],
     },
@@ -63,10 +71,60 @@ module.exports = {
   run: async (client, interaction) => {
     try {
       let mode = interaction.options.getSubcommand();
+      let supportServerButton = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel("サポートサーバーに参加する")
+          .setStyle(ButtonStyle.Link)
+          .setURL("https://discord.gg/uYYaVRuUuJ")
+      );
+
+      //時間かかる処理なので、defer
+      await interaction.deferReply({ ephemeral: true });
+
       if (mode === "on") {
+        let channel = interaction.options.getChannel("category");
+
+        //チャンネル設定が
+
+        //データベース設定
+        profileModel
+          .findById(interaction.guild.id)
+          .catch((err) => {
+            console.log(
+              `データベース更新時にエラーが発生しました。(場所：pomodoro/on)\n${err}`
+            );
+
+            return interaction.reply({
+              content:
+                "データベース関連の処理でエラーが発生しました。\nしばらく時間を空けて再度お試しいただくか、サポートサーバーにてお問い合わせください。",
+              ephemeral: true,
+              components: [supportServerButton],
+            });
+          })
+          .then((model) => {
+            model.pomodoro = true;
+            model.pomodoro_category = channel.id;
+            model.save().then(async () => {
+              return interaction.editReply({
+                embeds: [
+                  {
+                    title: "✅ 設定を更新しました！",
+                    color: 0x10ff00,
+                  },
+                ],
+              });
+            });
+          });
+
+        console.log("on");
       } else if (mode == "working_time") {
+        let workTime = interaction.options.getInteger("working_minute");
+        console.log("workingtime");
       } else if (mode == "breaking_time") {
+        let breakTime = interaction.options.getInteger("breaking_minute");
+        console.log("breaking time");
       } else if (mode == "off") {
+        console.log("off");
       }
     } catch (err) {
       const errorNotification = require("../errorNotification.js");
