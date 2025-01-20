b922c5be8c402c81c371a75ebca24beb908972fe
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-01-20 10:30:52 +0900

    一次schema変更

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -3,6 +3,22 @@ const mongoose = require("mongoose");
 const profileSchema = new mongoose.Schema(
   {
     _id: { type: String }, //サーバーID
+    sticky: {
+      status: { type: Boolean }, //スティッキーメッセージの有効/無効
+      channels: [
+        {
+          channelID: { type: String },
+          stickyMessage: { type: String },
+        },
+      ], //スティッキーメッセージのチャンネルIDとメッセージ
+    },
+    starboard: {
+      status: { type: Boolean }, //スターボードの有効/無効
+      channelID: { type: String }, //スターボードのチャンネルID
+      emojiID: { type: String }, //スターボードの投票カウントをする絵文字の設定
+      emojiCount: { type: Number }, //スターボードに表示するスターの数
+    },
+    // TODO: schemaを修正
     pomodoro: { type: Boolean }, //ポモドーロタイマーの有効/無効
     pomodoro_category: { type: String }, //ポモドーロタイマーが使用するカテゴリーID
     pomodoro_workingTime: { type: Number }, //ポモドーロタイマーの活動時間(分)
