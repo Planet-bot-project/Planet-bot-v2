ebb06943abbedf8496bb9f9994b726c8b3e1d125
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-05 23:35:48 +0900

    ピン留め機能にモーダルを追加し、メッセージと画像URLの入力を受け付けるように変更

--- commands/sticky.js
+++ commands/sticky.js
@@ -1,4 +1,12 @@
-const { SlashCommandBuilder, MessageFlags } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  MessageFlags,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder,
+} = require("discord.js");
+const profileSchema = require("../models/profileSchema.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,12 +29,53 @@ module.exports = {
 
       if (subcommand == "on") {
         // TODO: モーダルを表示して入力を受け付ける
+        profileSchema
+          .findById(interaction.guild.id)
+          .then(async (result) => {
+            if (result.sticky.status)
+              return interaction.reply({
+                content:
+                  "このチャンネルで既にピン留めが有効になっています。\n一度`/sticky off`を実行してピン留めを解除してから再度お試しください。",
+                flags: MessageFlags.Ephemeral,
+              });
+
+            // モーダルの表示
+            let modal = new ModalBuilder()
+              .setCustomId("sticky")
+              .setTitle("ピン留めの内容を設定");
+            let messageInput = new TextInputBuilder()
+              .setCustomId("stickyMessage")
+              .setLabel("ピン留めをするメッセージを入力してください。")
+              .setPlaceholder("ここにメッセージを入力してください。")
+              .setStyle(TextInputStyle.Paragraph)
+              .setMinLength(1)
+              .setMaxLength(250)
+              .setRequired(true);
+            let imageURLInput = new TextInputBuilder()
+              .setCustomId("stickyImageURL")
+              .setLabel(
+                "ピン留めする画像がある場合はそのURLを入力してください。"
+              )
+              .setPlaceholder("ここに画像のURLを入力してください。")
+              .setStyle(TextInputStyle.Short)
+              .setRequired(false);
+
+            let actionRow = new ActionRowBuilder().addComponents(messageInput);
+            modal.addComponents(actionRow);
+            actionRow = new ActionRowBuilder().addComponents(imageURLInput);
+            modal.addComponents(actionRow);
+
+            await interaction.showModal(modal);
+          })
+          .catch((err) => {
+            console.log(`Error: ${err}`);
+          });
       } else if (subcommand == "off") {
         // TODO: DBを更新して、ピン止め用メッセージを消せたら消す。
       } else {
         await interaction.reply({
           content:
-            "意図していないサブコマンドが使用されました。お手数ですがサポートサーバーまでお問い合わせください。",
+            "意図していないサブコマンドを検知しました。お手数ですがサポートサーバーまでお問い合わせください。",
           flags: MessageFlags.Ephemeral,
         });
       }
