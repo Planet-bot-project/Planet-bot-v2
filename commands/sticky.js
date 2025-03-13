e2deab05030d8f02dff7fae93e3610173fd31311
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-13 23:04:46 +0900

    stickyを無効化するコマンドを実装

--- commands/sticky.js
+++ commands/sticky.js
@@ -5,6 +5,8 @@ const {
   TextInputBuilder,
   TextInputStyle,
   ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
 } = require("discord.js");
 const profileSchema = require("../models/profileSchema.js");
 
@@ -19,8 +21,15 @@ module.exports = {
     )
     .addSubcommand((subcommand) =>
       subcommand
+        .setName("clear")
+        .setDescription("このチャンネルで有効なピン留めを削除・無効化します。")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
         .setName("off")
-        .setDescription("このチャンネルで有効なピン留めを削除します")
+        .setDescription(
+          "このサーバーの全てのチャンネルで有効なピン留めを削除・無効化します"
+        )
     ),
 
   run: async (client, interaction) => {
@@ -39,7 +48,7 @@ module.exports = {
             )
               return interaction.reply({
                 content:
-                  "このチャンネルで既にピン留めが有効になっています。\n一度`/sticky off`を実行してピン留めを解除してから再度お試しください。",
+                  "このチャンネルで既にピン留めが有効になっています。\n一度`/sticky clear`を実行してピン留めを解除してから再度お試しください。",
                 flags: MessageFlags.Ephemeral,
               });
 
@@ -86,8 +95,127 @@ module.exports = {
           .catch((err) => {
             console.log(`Error: ${err}`);
           });
+      } else if (subcommand == "clear") {
+        profileSchema
+          .findById(interaction.guild.id)
+          .then(async (result) => {
+            // 古いメッセージを削除出来そうならする
+            try {
+              const stickyChannel = result.sticky.channels.find(
+                (c) => c._id == interaction.channel.id
+              );
+              const oldMessage = await interaction.channel.messages.fetch(
+                stickyChannel.stickyMessage.oldMessageId
+              );
+              if (oldMessage) {
+                await oldMessage.delete();
+              }
+            } catch (err) {
+              console.log(err);
+              // 古いメッセージが見つからなかったり削除できない場合は、そのままメッセージ送信
+            }
+
+            // このチャンネルのデータを削除
+            result.sticky.channels.filter(
+              (c) => c._id !== interaction.channel.id
+            );
+            // 全てのチャンネルで無効化しているならoffに
+            if (!result.sticky.channels.length) result.sticky.status = false;
+
+            // 保存する
+            await result
+              .save()
+              .then(async () => {
+                return interaction.reply({
+                  content:
+                    "このチャンネルで有効な固定メッセージの削除に成功しました。",
+                  flags: MessageFlags.Ephemeral,
+                });
+              })
+              .catch((err) => {
+                let button = new ActionRowBuilder().addComponents(
+                  new ButtonBuilder()
+                    .setLabel("再招待はこちらから")
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(
+                      "https://discord.com/api/oauth2/authorize?client_id=949289830481821776&permissions=8&scope=bot%20applications.commands"
+                    )
+                );
+                return interaction.reply({
+                  content:
+                    "ピン留め作成時に、DB更新エラーが発生しました。お手数ですが、BOTを一度サーバーからkickしていただき、再招待をお願い致します。",
+                  components: [button],
+                  flags: MessageFlags.Ephemeral,
+                });
+              });
+          })
+          .catch((err) => {
+            let button = new ActionRowBuilder().addComponents(
+              new ButtonBuilder()
+                .setLabel("再招待はこちらから")
+                .setStyle(ButtonStyle.Link)
+                .setURL(
+                  "https://discord.com/api/oauth2/authorize?client_id=949289830481821776&permissions=8&scope=bot%20applications.commands"
+                )
+            );
+            return interaction.reply({
+              content:
+                "ピン留め作成時に、DB更新エラーが発生しました。お手数ですが、BOTを一度サーバーからkickしていただき、再招待をお願い致します。",
+              components: [button],
+              flags: MessageFlags.Ephemeral,
+            });
+          });
       } else if (subcommand == "off") {
-        // TODO: DBを更新して、ピン止め用メッセージを消せたら消す。
+        profileSchema
+          .findById(interaction.guild.id)
+          .then(async (result) => {
+            // このチャンネルのデータを削除
+            result.sticky.status = false;
+            result.sticky.channels = [];
+
+            // 保存する
+            await result
+              .save()
+              .then(() => {
+                return interaction.reply({
+                  content:
+                    "全チャンネルで有効な固定メッセージの削除に成功しました。\nなお、このコマンドを使用した場合はメッセージの自動削除が行われませんので、不要な場合はご自身で削除してください。",
+                  flags: MessageFlags.Ephemeral,
+                });
+              })
+              .catch((err) => {
+                let button = new ActionRowBuilder().addComponents(
+                  new ButtonBuilder()
+                    .setLabel("再招待はこちらから")
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(
+                      "https://discord.com/api/oauth2/authorize?client_id=949289830481821776&permissions=8&scope=bot%20applications.commands"
+                    )
+                );
+                return interaction.reply({
+                  content:
+                    "ピン留め作成時に、DB更新エラーが発生しました。お手数ですが、BOTを一度サーバーからkickしていただき、再招待をお願い致します。",
+                  components: [button],
+                  flags: MessageFlags.Ephemeral,
+                });
+              });
+          })
+          .catch((err) => {
+            let button = new ActionRowBuilder().addComponents(
+              new ButtonBuilder()
+                .setLabel("再招待はこちらから")
+                .setStyle(ButtonStyle.Link)
+                .setURL(
+                  "https://discord.com/api/oauth2/authorize?client_id=949289830481821776&permissions=8&scope=bot%20applications.commands"
+                )
+            );
+            return interaction.reply({
+              content:
+                "ピン留め作成時に、DB更新エラーが発生しました。お手数ですが、BOTを一度サーバーからkickしていただき、再招待をお願い致します。",
+              components: [button],
+              flags: MessageFlags.Ephemeral,
+            });
+          });
       } else {
         await interaction.reply({
           content:
