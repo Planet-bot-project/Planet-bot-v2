a18e257303f8aa72be6988dbe753b7526cb470b8
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-17 22:53:45 +0900

    入力チェック機能を追加し、エラーメッセージを改善

--- commands/starboard.js
+++ commands/starboard.js
@@ -1,4 +1,62 @@
-const { SlashCommandBuilder, ChannelType } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ChannelType,
+  PermissionsBitField,
+  Utils,
+  MessageFlags,
+} = require("discord.js");
+
+async function checkInput(sendChannel, emoji, emojiCount) {
+  let err = [];
+
+  // sendChannelのチェック
+  // チャンネルが存在するか確認
+  try {
+    if (!sendChannel) {
+      err.push("channelNotFound");
+    }
+
+    // ボットがそのチャンネルでメッセージを送信する権限があるか確認
+    let myPermissions = sendChannel.permissionsFor(
+      sendChannel.guild.members.me
+    );
+    if (!myPermissions.has(PermissionsBitField.Flags.ViewChannel)) {
+      err.push("doNotHaveViewChannel");
+    }
+    if (!myPermissions.has(PermissionsBitField.Flags.ReadMessageHistory)) {
+      err.push("doNotHaveViewMessageHistory");
+    }
+  } catch (err) {
+    err.push("canNotGetChannelInfo");
+  }
+  // emojiのチェック
+  try {
+    const isDefaultEmoji = Utils.parseEmoji(emoji).id === null;
+    const isCustomEmoji = sendChannel.guild.emojis.cache.some(
+      (e) => e.toString() === emoji
+    );
+
+    if (!isDefaultEmoji && !isCustomEmoji) {
+      err.push("invalidEmoji");
+    }
+  } catch (error) {
+    err.push("invalidEmoji");
+  }
+  // emojiCountのチェック
+  try {
+    if (typeof emojiCount != "number") {
+      err.push("invalidEmojiCount");
+    } else {
+      if (emojiCount > 1) {
+        err.push("invalidEmojiCount");
+      }
+    }
+  } catch (err) {
+    err.push("invalidEmojiCount");
+  }
+
+  return err;
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -25,7 +83,7 @@ module.exports = {
         )
         .addIntegerOption((option) =>
           option
-            .setName("emoji_count") // ここを修正
+            .setName("emoji_count")
             .setDescription(
               "メッセージについた絵文字の数がこの数を超えたときに、メッセージを転送する閾値を入力してください"
             )
@@ -33,7 +91,7 @@ module.exports = {
         )
         .addRoleOption((option) =>
           option
-            .setName("ignore_role") // ここを修正
+            .setName("ignore_role")
             .setDescription(
               "スターボードの絵文字数から無視するロールを設定します(任意)"
             )
@@ -45,6 +103,45 @@ module.exports = {
     ),
 
   run: async (client, interaction) => {
+    let subcommand = await interaction.options.getSubcommand();
+
+    if (subcommand == "on") {
+      let sendChannel = interaction.options.getChannel("send_channel");
+      let emoji = interaction.options.getString("emoji");
+      let emojiCount = interaction.options.getInteger("emoji_count");
+      let ignoreRole = interaction.options.getRole("ignore_role");
+
+      // 入力内容の確認
+      let errList = checkInput(sendChannel, emoji, emojiCount);
+      if (
+        (await errList).includes("channelNotFound") ||
+        (await errList).includes("doNotHaveViewChannel") ||
+        (await errList).includes("doNotHaveViewMessageHistory") ||
+        (await errList).includes("canNotGetChannelInfo")
+      ) {
+        return interaction.reply({
+          content: `誤ったチャンネル情報を確認しました。正しくチャンネルを指定しており、BOTにチャンネルの閲覧権限とメッセージ履歴の閲覧権限がある事を確認してください。\n-# support info: ${(
+            await errList
+          ).join(" and ")}`,
+          flags: MessageFlags.Ephemeral,
+        });
+      } else if ((await errList).includes("invalidEmoji")) {
+        return interaction.reply({
+          content: `誤った絵文字情報を確認しました。正しく絵文字が指定されているか確認してください。なお、他サーバーのカスタム絵文字は使用できません。\n-# support info: ${(
+            await errList
+          ).join(" and ")}`,
+          flags: MessageFlags.Ephemeral,
+        });
+      } else if ((await errList).includes("invalidEmojiCount")) {
+        return interaction.reply({
+          content: `誤った絵文字数を確認しました。1以上の自然数を半角数字で入力してください。\n-# support info: ${(
+            await errList
+          ).join(" and ")}`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    } else if (subcommand == "off") {
+    }
     await interaction.reply("test");
   },
 };
