6b980d35bfe98a06d922c929414eec244480dead
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-18 21:54:11 +0900

    メッセージリアクション追加時にプロファイルスキーマからデータを取得する処理を追加

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,4 +1,12 @@
+const profileSchema = require("../models/profileSchema");
+
 module.exports = async (client, reaction, user) => {
-    //db取得
-    //該当するメッセージは転送
+  //db取得
+  profileSchema
+    .findById(reaction.message.guild.id)
+    .then((result) => {
+      //該当するメッセージは転送
+      // 参考：https://qiita.com/norikmb/items/4fb47ac52ae5bc1ca14d
+    })
+    .catch((err) => {});
 };
