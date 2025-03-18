e45aecc93cb562cb559bbf55f4f2294fcefc0a06
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-18 18:22:44 +0900

    リアクションを取得するようにファイル作成と調整

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,4 @@
+module.exports = async (client, reaction, user) => {
+    //db取得
+    //該当するメッセージは転送
+};
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
   ],
 });
 const mongoose = require("mongoose");
