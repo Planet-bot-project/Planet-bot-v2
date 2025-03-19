cafc825478c0a7156815fdda63a3170292fd300e
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-03-19 20:27:33 +0900

    エラー処理を追加

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,3 +1,9 @@
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  MessageFlags,
+} = require("discord.js");
 const profileSchema = require("../models/profileSchema");
 // twemoji-parserから判定用の正規表現を取得(gオプション付き)
 const twemojiRegex = require("twemoji-parser/dist/lib/regex").default;
@@ -39,11 +45,28 @@ module.exports = async (client, reaction, user) => {
             }
           })
           .catch((err) => {
-            console.log(`err: ${err}`);
+            const errorNotification = require("../errorNotification.js");
+            errorNotification(client, interaction, err);
           });
       }
     })
     .catch((err) => {
-      console.log(err);
+      const errorNotification = require("../errorNotification.js");
+      errorNotification(client, interaction, err);
+
+      let button = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel("再招待はこちらから")
+          .setStyle(ButtonStyle.Link)
+          .setURL(
+            "https://discord.com/api/oauth2/authorize?client_id=949289830481821776&permissions=8&scope=bot%20applications.commands"
+          )
+      );
+      return reaction.message.reply({
+        content:
+          "メッセージリアクション受信時に、DB取得エラーが発生しました。お手数ですが、BOTを一度サーバーからkickしていただき、再招待をお願い致します。",
+        components: [button],
+        flags: MessageFlags.Ephemeral,
+      });
     });
 };
