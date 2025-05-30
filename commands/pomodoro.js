b6eea95e6ee32af0e1efcd0099a05321b67d61c3
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-05-30 21:55:17 +0900

    ポモドーロタイマーコマンドを改良し、設定オプションを追加

--- commands/pomodoro.js
+++ commands/pomodoro.js
@@ -11,25 +11,82 @@ const profileModel = require("../models/profileSchema");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    // TODO: このコマンドは、/settingsコマンドに移行する
     .setName("pomodoro")
-    .setDescription("⏱ポモドーロタイマーを設定します！")
+    .setDescription("⏱ポモドーロタイマーを管理します！")
     .addSubcommand((subcommand) =>
       subcommand
-        .setName("on")
-        .setDescription("ポモドーロタイマーを有効にします。")
-        .addChannelOption((option) =>
+        .setName("start")
+        .setDescription("ポモドーロタイマーを開始します。")
+        .addIntegerOption((option) =>
           option
-            .setName("category")
-            .setDescription("タイマーを使用するカテゴリーを設定してください。")
-            .addChannelTypes(ChannelType.GuildCategory)
+            .setName("work_time")
+            .setDescription("集中する時間を設定してください。(単位: 分)")
+            .setMinValue(1)
             .setRequired(true)
         )
+        .addIntegerOption((option) =>
+          option
+            .setName("break_time")
+            .setDescription("休憩する時間を設定してください。(単位: 分)")
+            .setMinValue(1)
+            .setRequired(true)
+        )
+        .addIntegerOption((option) =>
+          option
+            .setName("long_break_time")
+            .setDescription("長めの休憩時間を設定してください。(単位: 分)")
+            .setMinValue(1)
+            .setRequired(false)
+        )
+        .addBooleanOption((option) =>
+          option
+            .setName("voice_notification")
+            .setDescription(
+              "音声通知を有効にするかどうか。デフォルトは無効です。"
+            )
+            .setRequired(false)
+        )
     )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("off")
-        .setDescription("ポモドーロタイマーを無効にします。")
+    .addSubcommandGroup((subcommands) =>
+      subcommands
+        .setName("setting")
+        .setDescription("ポモドーロタイマーのデフォルト設定を変更します。")
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("default_work_time")
+            .setDescription("ポモドーロの作業時間を設定します。")
+            .addIntegerOption((option) =>
+              option
+                .setName("default_work_time")
+                .setDescription("作業時間を設定してください。(単位: 分)")
+                .setMinValue(1)
+                .setRequired(true)
+            )
+        )
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("default_break_time")
+            .setDescription("ポモドーロの休憩時間を設定します。")
+            .addIntegerOption((option) =>
+              option
+                .setName("default_break_time")
+                .setDescription("休憩時間を設定してください。(単位: 分)")
+                .setMinValue(1)
+                .setRequired(true)
+            )
+        )
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("default_long_break_time")
+            .setDescription("ポモドーロの長めの休憩時間を設定します。")
+            .addIntegerOption((option) =>
+              option
+                .setName("default_long_break_time")
+                .setDescription("長めの休憩時間を設定してください。(単位: 分)")
+                .setMinValue(1)
+                .setRequired(true)
+            )
+        )
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
