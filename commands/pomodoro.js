7b55250dfc0eeced748e309baa1306b50b6bd431
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-05-31 16:44:25 +0900

    ポモドーロタイマーの設定オプションを追加し、モード処理を改善

--- commands/pomodoro.js
+++ commands/pomodoro.js
@@ -91,6 +91,10 @@ module.exports = {
   run: async (client, interaction) => {
     try {
       let mode = interaction.options.getSubcommand();
+      mode != "start"
+        ? (mode = interaction.options.getSubcommandGroup())
+        : null;
+      let modeType = interaction.options.getSubcommand();
       let supportServerButton = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
           .setLabel("サポートサーバーに参加する")
@@ -98,77 +102,12 @@ module.exports = {
           .setURL(process.env.discord_bot_support)
       );
 
-      //時間かかる処理なので、defer
-      await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-
-      if (mode === "on") {
-        let channel = interaction.options.getChannel("category");
-
-        //データベース設定
-        profileModel
-          .findById(interaction.guild.id)
-          .catch((err) => {
-            console.log(
-              `データベース更新時にエラーが発生しました。(場所：pomodoro/on)\n${err}`
-            );
-
-            return interaction.reply({
-              content:
-                "データベース関連の処理でエラーが発生しました。\nしばらく時間を空けて再度お試しいただくか、サポートサーバーにてお問い合わせください。",
-              flags: MessageFlags.Ephemeral,
-              components: [supportServerButton],
-            });
-          })
-          .then((model) => {
-            // TODO: ポモドロはDBを使わない予定。そのため、この処理は削除する
-            model.pomodoro = true;
-            model.pomodoro_category = channel.id;
-            model.save().then(async () => {
-              return interaction.editReply({
-                embeds: [
-                  {
-                    title: "✅ ポモドーロタイマーを有効にしました！",
-                    description: `作成された「ロビー」というVCに参加すると、操作パネルが有効になり、ポモドーロタイマーを使用できるようになります。
-
-                    ※ カテゴリー名やチャンネル名などは変更しても問題ありません。
-                    ※ 別のチャンネルに変更する場合は再設定が必要です。`,
-                    color: 0x10ff00,
-                  },
-                ],
-              });
-            });
-          });
-      } else if (mode == "off") {
-        //データベース設定
-        profileModel
-          .findById(interaction.guild.id)
-          .catch((err) => {
-            console.log(
-              `データベース更新時にエラーが発生しました。(場所：pomodoro/on)\n${err}`
-            );
-
-            return interaction.reply({
-              content:
-                "データベース関連の処理でエラーが発生しました。\nしばらく時間を空けて再度お試しいただくか、サポートサーバーにてお問い合わせください。",
-              flags: MessageFlags.Ephemeral,
-              components: [supportServerButton],
-            });
-          })
-          .then((model) => {
-            // TODO: ポモドロはDBを使わない予定。そのため、この処理は削除する
-            model.pomodoro = false;
-            model.pomodoro_category = null;
-            model.save().then(async () => {
-              return interaction.editReply({
-                embeds: [
-                  {
-                    title: "✅ ポモドーロタイマーを無効にしました！",
-                    color: 0x10ff00,
-                  },
-                ],
-              });
-            });
-          });
+      if (mode == "start") {
+      } else if (mode === "setting") {
+        if (modeType == "default_work_time") {
+        } else if (modeType == "default_break_time") {
+        } else if (modeType == "default_long_break_time") {
+        }
       }
     } catch (err) {
       const errorNotification = require("../lib/errorNotification.js");
